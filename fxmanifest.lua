dbadad7cd5ed57a79ade288e5312957e57537ea5
Zalaya <enol.corzal@gmail.com>
2025-09-01 22:40:17 +0200

    feat: add the fivem resource template structure

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+name "<name>"
+description "<description>"
+author "<author>"
+version "<version>"
+
+client_scripts {
+  "./client/main.lua"
+}
+
+server_scripts {
+  "./server/main.lua"
+}
